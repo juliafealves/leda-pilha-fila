86d804a06bf0c52464544db0534fb743354cf92d
juliafealves <juliafealves>
2018-09-28 12:31:52 -0300

    Implementada a classe de pilha e os testes unitários.

--- src/main/java/adt/stack/StackImpl.java
+++ src/main/java/adt/stack/StackImpl.java
@@ -1,16 +1,30 @@
 package adt.stack;
 
+/**
+ * The interface of a generic stack. The queue is able to store any kind of data.
+ */
 public class StackImpl<T> implements Stack<T> {
 
     private T[] array;
     private int top;
 
+    /**
+     * Initialize a stack.
+     * @param size
+     */
     @SuppressWarnings("unchecked")
     public StackImpl(int size) {
         array = (T[]) new Object[size];
         top = -1;
     }
 
+    /**
+     * If the stack has elements, it removes the top of the stack and returns
+     * it; otherwise, it returns an exception.
+     *
+     * @return
+     * @throws StackUnderflowException
+     */
     @Override
     public T top() {
         T element = null;
@@ -32,11 +46,23 @@ public class StackImpl<T> implements Stack<T> {
         return top == -1;
     }
 
+    /**
+     * Returns true if the stack is full or false, otherwise.
+     *
+     * @return
+     */
     @Override
     public boolean isFull() {
         return top == (array.length - 1);
     }
 
+    /**
+     * Inserts a new element in the stack or returns an exception if the stack
+     * is full. Null elements are not allowed (the stack remains unchanged).
+     *
+     * @param element
+     * @throws StackOverflowException
+     */
     @Override
     public void push(T element) throws StackOverflowException {
         if (this.isFull()) {
@@ -46,12 +72,19 @@ public class StackImpl<T> implements Stack<T> {
         this.array[++this.top] = element;
     }
 
+    /**
+     * If the stack has elements, it removes the top of the stack and returns
+     * it; otherwise, it returns an exception.
+     *
+     * @return
+     * @throws StackUnderflowException
+     */
     @Override
     public T pop() throws StackUnderflowException {
         if (this.isEmpty()) {
             throw new StackUnderflowException();
         }
 
-        return this.array[this.top++];
+        return this.array[this.top--];
     }
 }
--- src/test/java/adt/stack/StudentStackTest.java
+++ src/test/java/adt/stack/StudentStackTest.java
@@ -3,6 +3,7 @@ package adt.stack;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,10 +12,12 @@ public class StudentStackTest {
     private Stack<Integer> stack1;
     private Stack<Integer> stack2;
     private Stack<Integer> stack3;
+    private Stack<Integer> stack;
 
     @Before
     public void setUp() throws StackOverflowException {
         getImplementations();
+        this.stack = new StackImpl<>(7);
 
         // Pilha com 3 elementos não cheia.
         stack1.push(1);
@@ -24,6 +27,7 @@ public class StudentStackTest {
         // Pilha com 2 elementos de tamanho 2, pilha cheia.
         stack2.push(1);
         stack2.push(2);
+
     }
 
     private void getImplementations() {
@@ -80,5 +84,93 @@ public class StudentStackTest {
     public void testPopComErro() throws StackUnderflowException {
         assertEquals(new Integer(3), stack1.pop()); // levanta excecao apenas se
         // stack1 for vazia
+    }
+
+    @Test
+    public void testInsertElement() throws StackOverflowException {
+        this.stack.push(1);
+        this.stack.push(2);
+        Assert.assertEquals(2, this.stack.top(), 0);
+    }
+
+    @Test (expected = StackOverflowException.class)
+    public void testInsertElementFull() throws StackOverflowException {
+        this.stack.push(1);
+        this.stack.push(2);
+        this.stack.push(3);
+        this.stack.push(4);
+        this.stack.push(5);
+        this.stack.push(6);
+        this.stack.push(7);
+        this.stack.push(8);
+    }
+
+    @Test
+    public void testRemoveElement() throws StackOverflowException, StackUnderflowException {
+        this.stack.push(1);
+        this.stack.push(2);
+        this.stack.push(3);
+        Assert.assertEquals(3, this.stack.pop(), 0);
+    }
+
+    @Test (expected = StackUnderflowException.class)
+    public void testRemoveElementStackEmpty() throws StackUnderflowException {
+        this.stack.pop();
+    }
+
+    @Test (expected = StackUnderflowException.class)
+    public void testRemoveElementStackEmpty2() throws StackUnderflowException, StackOverflowException {
+        this.stack.pop();
+        this.stack.push(2);
+        Assert.assertEquals(2, this.stack.top(), 0);
+    }
+
+    @Test
+    public void testTopElement() throws StackOverflowException, StackUnderflowException {
+        this.stack.push(1);
+        this.stack.push(2);
+        this.stack.push(3);
+        this.stack.pop();
+        Assert.assertEquals(2, this.stack.top(), 0);
+    }
+
+    @Test
+    public void testTopElementIsEmpty() throws StackOverflowException, StackUnderflowException {
+        Assert.assertNull(this.stack.top());
+    }
+
+    @Test
+    public void testTopElementIsFull() throws StackOverflowException {
+        Stack<Integer> stack = new StackImpl<>(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        Assert.assertEquals(3, stack.top(), 0);
+    }
+
+    @Test
+    public void testStackIsEmpty() {
+        Assert.assertTrue(this.stack.isEmpty());
+    }
+
+    @Test
+    public void testStackIsNotEmpty() throws StackOverflowException {
+        this.stack.push(2);
+        Assert.assertFalse(this.stack.isEmpty());
+    }
+
+    @Test
+    public void testStackIsNotFull() throws StackOverflowException {
+        this.stack.push(2);
+        Assert.assertFalse(this.stack.isFull());
+    }
+
+    @Test
+    public void testStackIsFull() throws StackOverflowException {
+        Stack<Integer> stack = new StackImpl<>(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        Assert.assertTrue(stack.isFull());
     }
 }
