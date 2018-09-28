f8cb5ed39721bcc239e177307cfa46d6492a1062
juliafealves <juliafealves>
2018-09-28 17:41:13 -0300

    Implementada a classe de fila e os testes unitários.

--- src/main/java/adt/queue/QueueImpl.java
+++ src/main/java/adt/queue/QueueImpl.java
@@ -1,49 +1,104 @@
 package adt.queue;
 
+/**
+ * The interface of a generic queue. The queue is able to store any kind of data.
+ */
 public class QueueImpl<T> implements Queue<T> {
 
-	private T[] array;
-	private int tail;
+    private T[] array;
+    private int tail;
 
-	@SuppressWarnings("unchecked")
-	public QueueImpl(int size) {
-		array = (T[]) new Object[size];
-		tail = -1;
-	}
+    /**
+     * Initialize the queue.
+     * @param size
+     */
+    @SuppressWarnings("unchecked")
+    public QueueImpl(int size) {
+        array = (T[]) new Object[size];
+        tail = -1;
+    }
 
-	@Override
-	public T head() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    /**
+     * If the queue has elements, it removes the oldest of the queue and returns
+     * it; otherwise, it throws an exception.
+     *
+     * @return
+     * @throws QueueUnderflowException
+     */
+    @Override
+    public T dequeue() throws QueueUnderflowException {
+        if (this.isEmpty()) {
+            throw new QueueUnderflowException();
+        }
 
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+        T element = this.head();
+        this.shiftLeft();
+        this.tail--;
 
-	@Override
-	public boolean isFull() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+        return element;
+    }
 
-	private void shiftLeft() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    /**
+     * Inserts a new element in the queue or returns an exception if the queue
+     * is full. Null elements are not allowed (the queue remains unchanged).
+     *
+     * @param element
+     * @throws QueueOverflowException
+     */
+    @Override
+    public void enqueue(T element) throws QueueOverflowException {
+        if (this.isFull()) {
+            throw new QueueOverflowException();
+        }
 
-	@Override
-	public void enqueue(T element) throws QueueOverflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+        this.array[++this.tail] = element;
+    }
 
-	@Override
-	public T dequeue() throws QueueUnderflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+    /**
+     * Returns (without removing) the oldest element of the queue; or
+     * null if the queue is empty.
+     *
+     * @return
+     */
+    @Override
+    public T head() {
+        T element = null;
 
+        if (!this.isEmpty()) {
+            element = this.array[0];
+        }
+
+        return element;
+    }
+
+    /**
+     * Returns true if the queue is empty; or false, otherwise.
+     *
+     * @return
+     */
+    @Override
+    public boolean isEmpty() {
+        return this.tail == -1;
+    }
+
+    /**
+     * Returns true if the queue is full; or false, otherwise.
+     *
+     * @return
+     */
+    @Override
+    public boolean isFull() {
+        return this.tail == (this.array.length - 1);
+    }
+
+    /**
+     * Execute shift of all elements in one position to left.
+     */
+    private void shiftLeft() {
+        if(this.tail >= 0){
+            for (int i = 0; i < tail; i++) {
+                this.array[i] = this.array[i + 1];
+            }
+        }
+    }
 }
--- src/test/java/adt/queue/StudentQueueTest.java
+++ src/test/java/adt/queue/StudentQueueTest.java
@@ -10,80 +10,163 @@ import org.junit.Test;
 
 public class StudentQueueTest {
 
-	public Queue<Integer> queue1;
-	public Queue<Integer> queue2;
-	public Queue<Integer> queue3;
+    public Queue<Integer> queue1;
+    public Queue<Integer> queue2;
+    public Queue<Integer> queue3;
 
-	@Before
-	public void setUp() throws QueueOverflowException {
+    private Queue<Integer> queue;
 
-		getImplementations();
+    @Before
+    public void setUp() throws QueueOverflowException {
+        getImplementations();
+        queue = new QueueImpl<>(7);
 
-		// Fila com 3 elementos não cheia.
-		queue1.enqueue(1);
-		queue1.enqueue(2);
-		queue1.enqueue(3);
+        // Fila com 3 elementos não cheia.
+        queue1.enqueue(1);
+        queue1.enqueue(2);
+        queue1.enqueue(3);
 
-		// Fila com 2 elementos de tamanho 2. Fila cheia.
-		queue2.enqueue(1);
-		queue2.enqueue(2);
+        // Fila com 2 elementos de tamanho 2. Fila cheia.
+        queue2.enqueue(1);
+        queue2.enqueue(2);
 
-	}
+    }
 
-	private void getImplementations() {
-		// TODO O aluno deve ajustar aqui para instanciar sua implementação
-		queue1 = null;
-		queue2 = null;
-		queue3 = null;
-	}
+    private void getImplementations() {
+        // TODO O aluno deve ajustar aqui para instanciar sua implementação
+        queue1 = new QueueImpl<>(4);
+        queue2 = new QueueImpl<>(2);
+        queue3 = new QueueImpl<>(3);
+    }
 
-	// MÉTODOS DE TESTE
-	@Test
-	public void testHead() {
-		assertEquals(new Integer(1), queue1.head());
-	}
+    // MÉTODOS DE TESTE
+    @Test
+    public void testHead() {
+        assertEquals(new Integer(1), queue1.head());
+    }
 
-	@Test
-	public void testIsEmpty() {
-		assertFalse(queue1.isEmpty());
-		assertTrue(queue3.isEmpty());
-	}
+    @Test
+    public void testIsEmpty() {
+        assertFalse(queue1.isEmpty());
+        assertTrue(queue3.isEmpty());
+    }
 
-	@Test
-	public void testIsFull() {
-		assertFalse(queue1.isFull());
-	}
+    @Test
+    public void testIsFull() {
+        assertFalse(queue1.isFull());
+    }
 
-	@Test
-	public void testEnqueue() {
-		try {
-			queue1.enqueue(new Integer(5));
-		} catch (QueueOverflowException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    @Test
+    public void testEnqueue() {
+        try {
+            queue1.enqueue(new Integer(5));
+        } catch (QueueOverflowException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	@Test(expected = QueueOverflowException.class)
-	public void testEnqueueComErro() throws QueueOverflowException {
-		queue1.enqueue(new Integer(5)); // vai depender do tamanho que a fila
-										// foi iniciada!!!
-	}
+    @Test(expected = QueueOverflowException.class)
+    public void testEnqueueComErro() throws QueueOverflowException {
+        queue1.enqueue(new Integer(5)); // vai depender do tamanho que a fila
+        // foi iniciada!!!
+    }
 
-	@Test
-	public void testDequeue() {
-		try {
-			assertEquals(new Integer(1), queue1.dequeue());
-		} catch (QueueUnderflowException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    @Test
+    public void testDequeue() {
+        try {
+            assertEquals(new Integer(1), queue1.dequeue());
+        } catch (QueueUnderflowException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	@Test(expected = QueueUnderflowException.class)
-	public void testDequeueComErro() throws QueueUnderflowException {
-		assertEquals(new Integer(1), queue1.dequeue()); // vai depender do
-														// tamanho que a fial
-														// foi iniciada!!!
-	}
+    @Test(expected = QueueUnderflowException.class)
+    public void testDequeueComErro() throws QueueUnderflowException {
+        assertEquals(new Integer(1), queue1.dequeue()); // vai depender do
+        // tamanho que a fial
+        // foi iniciada!!!
+    }
+
+    @Test
+    public void testQueueIsEmpty() {
+        Assert.assertTrue(this.queue.isEmpty());
+    }
+
+    @Test
+    public void testQueueIsNotEmpty() throws QueueOverflowException {
+        this.queue.enqueue(2);
+        Assert.assertFalse(this.queue.isEmpty());
+    }
+
+    @Test
+    public void testQueueIsFull() throws QueueOverflowException {
+        Queue<Integer> queue = new QueueImpl<>(2);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        Assert.assertTrue(queue.isFull());
+    }
+
+    @Test
+    public void testQueueIsNotFull() {
+        Assert.assertFalse(this.queue.isFull());
+    }
+
+    @Test
+    public void testQueueHeadIsEmpty() {
+        Assert.assertNull(this.queue.head());
+    }
+
+    @Test
+    public void testQueueHead() throws QueueOverflowException {
+        this.queue.enqueue(3);
+        this.queue.enqueue(2);
+        this.queue.enqueue(1);
+        Assert.assertEquals(3, this.queue.head(), 0);
+    }
+
+    @Test
+    public void testQueueEnqueue() throws QueueOverflowException {
+        this.queue.enqueue(1);
+        this.queue.enqueue(2);
+        this.queue.enqueue(3);
+        Assert.assertEquals(1, this.queue.head(), 0);
+    }
+
+    @Test(expected = QueueOverflowException.class)
+    public void testQueueEnqueueFull() throws QueueOverflowException {
+        this.queue.enqueue(1);
+        this.queue.enqueue(2);
+        this.queue.enqueue(3);
+        this.queue.enqueue(4);
+        this.queue.enqueue(5);
+        this.queue.enqueue(6);
+        this.queue.enqueue(7);
+        this.queue.enqueue(8);
+    }
+
+    @Test
+    public void testQueueDequeueHead() throws QueueOverflowException, QueueUnderflowException {
+        this.queue.enqueue(1);
+        this.queue.enqueue(2);
+        this.queue.dequeue();
+        this.queue.dequeue();
+        this.queue.enqueue(3);
+        Assert.assertEquals(3, this.queue.head(), 0);
+    }
+
+    @Test
+    public void testQueueDequeue() throws QueueOverflowException, QueueUnderflowException {
+        this.queue.enqueue(1);
+        this.queue.enqueue(2);
+        this.queue.dequeue();
+        this.queue.enqueue(3);
+        Assert.assertEquals(2, this.queue.head(), 0);
+    }
+
+    @Test(expected = QueueUnderflowException.class)
+    public void testQueueDequeueEmpty() throws QueueUnderflowException {
+        this.queue.dequeue();
+    }
 }
